c3be9e966030b5199f63f5eedcd1af49501e1b62
Renaud Salliot <100938205+oOoRenaudoOo@users.noreply.github.com>
2022-03-28 06:15:13 -0500

    Add files via upload

---
+++ admin/crud.php
@@ -0,0 +1,45 @@
+<?php
+
+include ("config.php");
+
+
+if (isset($_GET["action"])){
+   
+    switch($_GET["action"]) {
+        
+        case "ajouter":
+
+            $maRequete = "INSERT INTO `plantes` (`id`, `nom`,`description`, `prix`, `dateIns`) VALUES (NULL, '".$_POST["nom"]."', '".$_POST["description"]."', '".$_POST["prix"]."', CURRENT_TIMESTAMP)";
+
+        break;
+
+        case "supprimer":
+
+            $maRequete = "DELETE FROM plantes WHERE id = ".$_GET["id"];
+
+        break;
+           
+        case "modifier":
+
+            $maRequete = "";
+            /*   $maRequete = "UPDATE `plantes` SET `description` = '$_POST["description"]', `prix` = '$_POST["prix"]' WHERE `plantes`.`id` = $_POST["id"]";
+            */
+        break;
+
+        default :
+           
+            echo ($maRequete);
+
+        break;
+
+    }
+
+    $resultat = $mysqli->query($maRequete);
+
+}
+    // redirection par defaut
+
+    header("location: ./index.php");
+
+
+?>
